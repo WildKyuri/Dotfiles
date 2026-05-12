3f7e623a7404e705d844b274153e49c51df60cfc
WildKyuri <haziel.querol@gmail.com>
2026-05-12 13:05:26 -0600

    keymaps relocated

--- configs/LazyVim/lua/config/keymaps/keys-plugins.lua
+++ configs/LazyVim/lua/config/keymaps/keys-plugins.lua
@@ -1,102 +1,3 @@
--- local keymap = vim.keymap
--- local opts = { noremap = true, silent = true }
--- local snacks = require("snacks")
--- local ls = require("luasnip")
--- local dap = require("dap")
---
--- --  Treesitter
--- keymap.set("n", "<Leader>ft", function()
---   snacks.picker.treesitter({
---     finder = "treesitter_symbols",
---     tree = true,
---     filter = {
---       default = {
---         "Class",
---         "Enum",
---         "Field",
---         "Function",
---         "Method",
---         "Module",
---         "Namespace",
---         "Struct",
---         "Trait",
---       },
---       -- set to `true` to include all symbols
---       markdown = true,
---       help = true,
---     },
---   })
--- end, opts)
---
--- -- Oil
--- keymap.set("n", "-", ":Oil<Return>", opts)
---
--- -- Atajo para abrir o actualizar la vista previa de Markdown
--- keymap.set("n", "<Leader>m", ":MarkdownPreviewToggle<CR>", opts)
---
--- -- Five Server
--- keymap.set("n", "<Leader>fs", ":FiveServer start<CR>", opts)
--- keymap.set("n", "<Leader>fss", ":FiveServer stop<CR>", opts)
---
--- -- Databases
--- keymap.set("n", "<Leader>db", "<cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>")
---
--- -- Aerial
--- keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", opts)
--- keymap.set("n", "{", "<cmd>AerialPrev<CR>", opts)
--- keymap.set("n", "}", "<cmd>AerialNext<CR>", opts)
---
--- -- Saltar en los snippets
--- keymap.set({ "i", "s" }, "<c-d>", function()
---   if ls.expand_or_jumpable() then
---     ls.expand_or_jump()
---   end
--- end, { silent = true })
---
--- -- <ctrl-a> is my jump backwards key.
--- -- this always moves to the previous item within the snippet
--- keymap.set({ "i", "s" }, "<c-a>", function()
---   if ls.jumpable(-1) then
---     ls.jump(-1)
---   end
--- end, { silent = true })
---
--- -- Toggle para signature help
--- local function toggle_signature_help()
---   -- Busca si ya hay una ventana flotante de signature_help abierta
---   for _, win in ipairs(vim.api.nvim_list_wins()) do
---     local buf = vim.api.nvim_win_get_buf(win)
---     if vim.bo[buf].filetype == "lspinfo" then
---       vim.api.nvim_win_close(win, true)
---       return
---     end
---   end
---   -- Si no la encontramos, abrimos signature_help
---   vim.lsp.buf.signature_help()
--- end
---
--- -- Asignamos la misma tecla para togglear
--- keymap.set("i", "<A-d>", toggle_signature_help, { silent = true, desc = "Toggle signature help (insert)" })
--- keymap.set("n", "<A-d>", toggle_signature_help, { silent = true, desc = "Toggle signature help (normal)" })
---
--- -- Dap nvim
--- keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })                  --> Iniciar/Continuar
--- keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })                     --> Ejecuta sin entrar en funciones
--- keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })                     --> Entra dentro de funciones
--- keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })                       --> Sale de la funcion actual
--- keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" }) --> Breakpoint
--- keymap.set("n", "<leader>B", function()                                                    --> Breakpoint condicional
---   dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
--- end, { desc = "Debug: Set Conditional Breakpoint" })
---
--- -- Gitsigns
--- keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
---
--- -- Snipe
--- keymap.set("n", "<S-l>", function()
---   require("snipe").open_buffer_menu()
--- end, opts)
-
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 local snacks = require("snacks")
@@ -118,21 +19,6 @@ local function close_lsp_floats()
   end
 end
 
-local function toggle_signature_help()
-  -- Si ya hay una ventana flotante abierta, la cerramos
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
-
-    if config.relative ~= "" then
-      pcall(vim.api.nvim_win_close, win, true)
-      return
-    end
-  end
-
-  -- Si no hay flotante, abrimos signature help
-  vim.lsp.buf.signature_help()
-end
-
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local bufnr = event.buf
@@ -146,42 +32,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
       }
     end
 
-    -- Hover / documentación
-    keymap.set("n", "K", vim.lsp.buf.hover, lsp_opts("LSP: Hover documentation"))
+    -- hover / documentación
+    keymap.set("n", "K", vim.lsp.buf.hover, lsp_opts("lsp: hover documentation"))
 
-    -- Signature help
-    keymap.set("n", "K", vim.lsp.buf.hover, lsp_opts("LSP: Hover documentation"))
+    -- signature help
+    keymap.set("n", "K", vim.lsp.buf.hover, lsp_opts("lsp: hover documentation"))
 
     keymap.set({ "n", "i" }, "<leader>sdd", function()
       vim.lsp.buf.signature_help()
-    end, lsp_opts("LSP: Signature help"))
+    end, lsp_opts("lsp: signature help"))
 
-    -- Navegación
-    keymap.set("n", "sf", vim.lsp.buf.definition, lsp_opts("LSP: Go to definition"))
-    keymap.set("n", "gD", vim.lsp.buf.declaration, lsp_opts("LSP: Go to declaration"))
-    keymap.set("n", "gi", vim.lsp.buf.implementation, lsp_opts("LSP: Go to implementation"))
-    keymap.set("n", "gr", vim.lsp.buf.references, lsp_opts("LSP: References"))
+    -- navegación
+    keymap.set("n", "<leader>cf", vim.lsp.buf.definition, lsp_opts("Código: ir a definición"))
+    keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, lsp_opts("Código: ir a declaración"))
+    keymap.set("n", "<leader>ci", vim.lsp.buf.implementation, lsp_opts("Código: ir a implementación"))
+    keymap.set("n", "<leader>cr", vim.lsp.buf.references, lsp_opts("Código: referencias"))
 
-    -- Acciones
-    keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, lsp_opts("LSP: Code action"))
-    keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, lsp_opts("LSP: Code action"))
-    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, lsp_opts("LSP: Rename symbol"))
-
-    -- Diagnósticos
-    keymap.set("n", "<leader>e", vim.diagnostic.open_float, lsp_opts("LSP: Show diagnostic"))
-    keymap.set("n", "[d", vim.diagnostic.goto_prev, lsp_opts("LSP: Previous diagnostic"))
-    keymap.set("n", "]d", vim.diagnostic.goto_next, lsp_opts("LSP: Next diagnostic"))
-
-    -- Cerrar flotantes
-    keymap.set("n", "<leader>lc", close_lsp_floats, lsp_opts("LSP: Close floating windows"))
-
-    -- Inlay hints toggle
-    keymap.set("n", "<leader>lh", function()
-      if vim.lsp.inlay_hint then
-        local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
-        vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
-      end
-    end, lsp_opts("LSP: Toggle inlay hints"))
+    -- acciones
+    keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, lsp_opts("lsp: code action"))
+    keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, lsp_opts("lsp: code action"))
   end,
 })
 
